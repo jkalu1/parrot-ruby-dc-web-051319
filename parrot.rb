67ddd3983b04b0768629d8172fcc18c6dc493cab
upbeat-changelog-8230 <jkalu1000@gmail.com>
2019-04-20 19:12:01 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
+def #parrot
   put Squawk!
 end
-  "What noise does a parrot make (put parrot)"
+  "What noise does a parrot make (put #parrot)"
